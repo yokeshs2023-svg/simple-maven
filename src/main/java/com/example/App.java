75d63234926c4e1ac1af6f63c020c470df8896c1
yokeshs2023-svg <yokesh.s2023@vitstudent.ac.in>
2026-02-23 10:46:04 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,6 @@
 package com.example;
 public class App {
- public static void main(String[] args) {
- System.out.println("Hello, Maven World!");
- int a = 10;
- int b = 20;
- int sum = add(a, b);
- System.out.println("Sum of " + a + " and " + b + " is: " + sum);
- }
- public static int add(int x, int y) {
- return x + y;
- }
+public int add(int a, int b) {
+return a + b;
+}
 }
